998a73dbb6d9af705f140b13132db694135ae054
Goryachev Vasily <goryachev.vasily.mpc@gmail.com>
2021-05-15 14:46:59 +0400

    Add assets.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Weather forecast will be soon...
+		<div className="app">
+			<header className="app__header">
+				<p className="app__title">
+					<span>Weather</span>
+					<span>forecast</span>
 				</p>
 			</header>
+
+			<main className="app__main">
+			</main>
+
+			<footer className="app__footer">
+				C ЛЮБОВЬЮ ОТ MERCURY DEVELOPMENT
+			</footer>
 		</div>
 	);
 }
